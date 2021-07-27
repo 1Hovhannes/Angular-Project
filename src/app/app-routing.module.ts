b4415ec4f1a3df021c27b8aac8cfe109a9fa74f2
1Hovhannes <hovhan1993@gmail.com>
2021-07-27 12:41:49 +0400

    convert html css in angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MyServicesComponent } from './my-services/my-services.component';
+import { NewsComponent } from './news/news.component';
+import { TrainerComponent } from './trainer/trainer.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Services', component: MyServicesComponent },
+  { path: 'News', component: NewsComponent },
+  { path: 'Trainer', component: TrainerComponent },
+  { path: 'Gallery', component: GalleryComponent },
+  { path: 'Contact US', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MyServicesComponent } from './my-services/my-services.component';
+import { NewsComponent } from './news/news.component';
+import { TrainerComponent } from './trainer/trainer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    GalleryComponent,
+    HeaderComponent,
+    HomeComponent,
+    MyServicesComponent,
+    NewsComponent,
+    TrainerComponent
   ],
   imports: [
     BrowserModule,
